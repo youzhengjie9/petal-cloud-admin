b8203800045f77874bf5a055c789753f95d18653
ms666 <1550324080@qq.com>
2023-06-02 09:20:17 +0800

    更新了一些代码

---
+++ petal-auth/src/main/java/com/petal/auth/PetalAuthApplication.java
@@ -0,0 +1,16 @@
+package com.petal.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PetalAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetalAuthApplication.class,args);
+
+    }
+
+}
